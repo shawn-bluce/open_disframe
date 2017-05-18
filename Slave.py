49d49317d5a5d5fd7b259bd2f5fcf8a471197dc1
shawnx <shawnbluce@gmail.com>
2017-05-18 19:26:41 +0800

    Update Slave.py
    更新注释信息

--- Slave.py
+++ Slave.py
@@ -16,9 +16,11 @@ BSIZE = 10240
 ADDRESS = None
 
 
-# 获取当前Slave的ID
-# ID由第一次调用的时候生成，以后直接获取值
 def get_id() -> str:
+    """
+    获取当前Slave的ID，ID由首次调用时生成
+    :return: 
+    """
     global MY_ID
 
     if MY_ID is None:
@@ -29,14 +31,24 @@ def get_id() -> str:
     return MY_ID
 
 
-# 设置Master节点的IP和端口号
 def setup(host_ip: str, port: int) -> None:
+    """
+    设置Master节点的IP和端口号，用于初始化
+    :param host_ip: Master的IP
+    :param port: Master的端口
+    :return: None
+    """
     global ADDRESS
     ADDRESS = (host_ip, port)
 
 
-# 发送一条请求到Master节点，将响应字符串作为返回值
 def send_request(method: str, message_list=None) -> str or None:
+    """
+    发送一条请求到Master节点，将响应字符串作为返回值
+    :param method: 该请求的方法
+    :param message_list: 请求携带的消息列表
+    :return: 从Master端接收的响应信息和时间戳
+    """
     global ADDRESS
 
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,18 +69,24 @@ def send_request(method: str, message_list=None) -> str or None:
     # return hashlib.md5(response).hexdigest() + "  time: " + now_time
 
 
-# 构建与Master通信用的Json数据，将字符串格式的Json数据作为返回值
-# message_list是列表
 def build_request_json(method: str, message_list: str or None) -> str:
+    """
+    :param method: 该请求的方法
+    :param message_list: 请求携带的消息列表
+    :return: 构建好的用于和Master通信的Json数据
+    """
     json_data = {"id": get_id(), "time": str(time.time()), "method": method,
                  "message_list": build_list_json(message_list)}
     json_data = json.dumps(json_data)
     return json_data
 
 
-# 构建submit_message请求用的消息列表，将列表转成字符串并返回
-# message需要是用字符串列表
 def build_list_json(message_list: list or None) -> str:
+    """
+    构建submit_message请求用的消息列表，将列表转成字符串并返回
+    :param message_list: 传入的消息列表
+    :return: 构建好的消息Json
+    """
     count = 0
     json_data = {}
     if message_list is None:
@@ -79,37 +97,28 @@ def build_list_json(message_list: list or None) -> str:
     return json.dumps(json_data)
 
 
-# 每6秒发送一波存活证明，以此续命
-# 单独开一个线程，死循环执行这个方法
 def keep_alive() -> None:
+    """
+    每6秒发送一波存活证明，以此续命，单独开一个线程，死循环执行这个方法
+    :return: 
+    """
     while True:
         time.sleep(6)
         send_request("heartbeat")
         # print('send heartbeat package to keep alive')
 
 
-# 测试用，生成随机字符串
-def random_str(randomlength=8):
-    res = str()
-    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
-    length = len(chars) - 1
-    for i in range(randomlength):
-        res += chars[random.randint(0, length)]
-    return res
-
 if __name__ == '__main__':
-    setup('localhost', 4022)
+    # setup('localhost', 4022)
+    setup('115.29.52.14', 4022)
     print(send_request("handshake"))
     threading.Thread(target=keep_alive, args=()).start()
 
-    # 测试用，循环提交消息
     # while True:
     #     print(send_request('submit_message', [random_str(7), random_str(7), random_str(7),
     #                                           random_str(7), random_str(7), random_str(7),
     #                                           random_str(7)]))
     #     time.sleep(0.6)
-
-    # 测试用，循环提取消息
     while True:
         print(send_request('get_message'))
         time.sleep(1.8)
